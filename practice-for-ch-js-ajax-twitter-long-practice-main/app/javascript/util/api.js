e97eb9a039092f2a5fc8a96325aa3c4002e35d07
ishsgit <aishtahad25@gmail.com>
2022-11-09 21:48:24 -0500

    working api.js

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -1,11 +1,21 @@
 const csrfToken = document.querySelector("meta[name=csrf-token]").content;
-export const foo = "bar";
+// export const foo = "bar";
 
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
+    " x-CSRF-Token": csrfToken,
+    Accept: "application/json",
     ...options.headers,
   };
 
   return await fetch(url, options);
+}
+
+export function followUser(id) {
+  return customFetch(`/users/${id}/follow`, { method: "post" });
+}
+
+export function unfollowUser(id) {
+  return customFetch(`/users/${id}/follow`, { method: "delete" });
 }
